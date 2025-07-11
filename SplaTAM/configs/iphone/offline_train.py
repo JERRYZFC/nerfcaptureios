b713920b6ba0bc8a50a84f34d4439f4f35c3faf3
JERRYZFC <shrl007@163.com>
2025-07-11 15:03:27 +0800

    成功将离线配置文件 SplaTAM/configs/iphone/offline_train.py 进行了全面重构。现在，该配置文件的结构和参数都与在线模式保持一致，并且已经更新为您指定的离线数据路径。最初的 KeyError 问题已经解决，您可以重新运行您的脚本了。

--- SplaTAM/configs/iphone/offline_train.py
+++ SplaTAM/configs/iphone/offline_train.py
@@ -1,4 +1,10 @@
 # Config for offline training using data captured from the iOS app
+import os
+from os.path import join as p_join
+
+# Iteration counts
+tracking_iters = 10
+mapping_iters = 120
 
 config = {
     "workdir": "./work/offline_iphone",
@@ -13,17 +19,14 @@ config = {
     "scene_radius_depth_ratio": 3,
     "mean_sq_dist_method": "projective",
     "gaussian_distribution": "anisotropic",
+    "report_iter_progress": False,
     "load_checkpoint": False,
     "checkpoint_time_idx": 500,
     "save_checkpoints": True,
     "checkpoint_interval": 100,
     "data": {
-        # IMPORTANT: Change this path to the parent directory of your captured sessions.
-        # For example: "/path/to/your/captures"
-        "basedir": "path/to/your/captures/parent/directory",
-        # IMPORTANT: Change this to the name of the specific session folder you want to train on.
-        # For example: "240711133000"
-        "sequence": "your_session_folder_name",
+        "basedir": "F:/nerfcap",
+        "sequence": "nerf250711143013",
         "dataset_name": "nerfcapture",
         "desired_image_height": 192,
         "desired_image_width": 256,
@@ -35,63 +38,76 @@ config = {
         "num_frames": -1,
     },
     "tracking": {
-        "use_gt_poses": True,  # Use the poses from the capture app
-        "num_iters": 10, # Less iters since we are using GT poses
-        "lrs": {
-            'cam_unnorm_rots': 0.0, # No need to optimize camera poses if using GT
-            'cam_trans': 0.0,
-        },
-        "loss_weights": {
-            'im': 0.0, # No need for loss if not optimizing
-            'depth': 0.0,
-        },
+        "use_gt_poses": True,
+        "forward_prop": True,
+        "visualize_tracking_loss": False,
+        "num_iters": tracking_iters,
         "use_sil_for_loss": False,
         "sil_thres": 0.99,
         "use_l1": True,
+        "use_depth_loss_thres": False,
+        "depth_loss_thres": 100000,
         "ignore_outlier_depth_loss": False,
-        "forward_prop": True,
+        "loss_weights": {
+            "im": 0.0,
+            "depth": 0.0,
+        },
+        "lrs": {
+            "means3D": 0.0,
+            "rgb_colors": 0.0,
+            "unnorm_rotations": 0.0,
+            "logit_opacities": 0.0,
+            "log_scales": 0.0,
+            "cam_unnorm_rots": 0.0,
+            "cam_trans": 0.0,
+        },
     },
     "mapping": {
-        "num_iters": 120,
+        "num_iters": mapping_iters,
         "add_new_gaussians": True,
         "sil_thres": 0.5,
         "use_l1": True,
+        "ignore_outlier_depth_loss": False,
+        "use_sil_for_loss": False,
+        "use_uncertainty_for_loss_mask": False,
+        "use_uncertainty_for_loss": False,
+        "use_chamfer": False,
         "use_gaussian_splatting_densification": True,
         "prune_gaussians": True,
         "loss_weights": {
-            'im': 0.5,
-            'depth': 1.0,
+            "im": 0.5,
+            "depth": 1.0,
         },
         "lrs": {
-            'means3D': 0.0001,
-            'rgb_colors': 0.0025,
-            'unnorm_rotations': 0.001,
-            'logit_opacities': 0.05,
-            'log_scales': 0.001,
-            'cam_unnorm_rots': 0.0000,
-            'cam_trans': 0.0000,
+            "means3D": 0.0001,
+            "rgb_colors": 0.0025,
+            "unnorm_rotations": 0.001,
+            "logit_opacities": 0.05,
+            "log_scales": 0.001,
+            "cam_unnorm_rots": 0.0000,
+            "cam_trans": 0.0000,
         },
         "pruning_dict": {
-            'start_after': 0,
-            'remove_big_after': 0,
-            'stop_pruning_after': 20,
-            'prune_every': 20,
-            'removal_opacity_threshold': 0.4,
-            'final_removal_opacity_threshold': 0.6,
-            'reset_opacities': True,
-            'reset_opacities_every': 500,
+            "start_after": 0,
+            "remove_big_after": 0,
+            "stop_pruning_after": 20,
+            "prune_every": 20,
+            "removal_opacity_threshold": 0.4,
+            "final_removal_opacity_threshold": 0.6,
+            "reset_opacities": True,
+            "reset_opacities_every": 500,
         },
         "densify_dict": {
-            'start_after': 100,
-            'remove_big_after': 3000,
-            'stop_densifying_after': 5000,
-            'densify_every': 100,
-            'grad_thresh': 0.0002,
-            'num_to_split_into': 2,
-            'removal_opacity_threshold': 0.4,
-            'final_removal_opacity_threshold': 0.7,
-            'reset_opacities': True,
-            'reset_opacities_every': 3000,
+            "start_after": 100,
+            "remove_big_after": 3000,
+            "stop_densifying_after": 5000,
+            "densify_every": 100,
+            "grad_thresh": 0.0002,
+            "num_to_split_into": 2,
+            "removal_opacity_threshold": 0.4,
+            "final_removal_opacity_threshold": 0.7,
+            "reset_opacities": True,
+            "reset_opacities_every": 3000,
         },
     },
     "use_wandb": False,
